d24bb7bf980c13a44704d3052ce89a798bd87adc
gina.feng <gina.feng@wehotelglobal.com>
2021-01-17 23:23:13 +0800

    feat(exam):增加exam page

--- server/routes/wechat.js
+++ server/routes/wechat.js
@@ -1,4 +1,4 @@
-import {controller, get, log} from '../decorator/router'
+import {controller, get, post, log} from '../decorator/router'
 import * as wechat from '../controller/wechat'
 import wechatMiddle from '../wechat-lib/middleware'
 import reply from '../wechat/reply'
@@ -7,6 +7,13 @@ import config from '../config'
 @controller('')
 export class WxController {
  @get('/wechat-hear')
+ @log
+ async wxHear (ctx, next) {
+   const middle = wechatMiddle(config.wechat, reply)
+   await middle(ctx, next)
+ }
+ 
+ @post('/wechat-hear')
  @log
  async wxHear (ctx, next) {
    const middle = wechatMiddle(config.wechat, reply)
